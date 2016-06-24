5aac39fbe11506d3bf5870a69ba9ab8eaf99ab7c
GPerelman <3123958139@qq.com>
2016-06-24 14:30:45 +0800

    http://www.cnblogs.com/blog-3123958139

--- dll_.cpp
+++ dll_.cpp
@@ -10,8 +10,7 @@ using namespace std;
 IO_DLL_ void hello_(void) {
     cout << "函数调用方法如下：" << endl;
     cout << "函数格式@0：void hello_(void)" << endl;
-    cout << "函数格式@1：int w_rate_ma_("
-            "                           // double stock_index_[], \n"
+    cout << "函数格式@1：int w_rate_ma_(// double stock_index_[], \n"
             "                           // char *stock_date_[], \n"
             "                           double stock_open_[], \n"
             "                           double stock_high_[], \n"
@@ -22,8 +21,7 @@ IO_DLL_ void hello_(void) {
             "                           int ma_len_)" << endl;
 }
 
-IO_DLL_ int w_rate_ma_(
-        // double stock_index_[],
+IO_DLL_ int w_rate_ma_(// double stock_index_[],
         // char *stock_date_[],
                        double stock_open_[],
                        double stock_high_[],
@@ -34,6 +32,7 @@ IO_DLL_ int w_rate_ma_(
                        int ma_len_) {
 
     // 计算加权价
+    cout << "计算 w_price_ ..." << endl;
     double w_price_[stock_len_];
     for (int i = 0; i < stock_len_; ++i) {
         w_price_[i] = stock_open_[i] * 2
@@ -44,30 +43,34 @@ IO_DLL_ int w_rate_ma_(
     }
 
     // 计算涨跌幅
-    double w_rate_[stock_len_];
-    w_rate_[0] = 0;
+    cout << "计算 w_price_rate_ ..." << endl;
+    double w_price_rate_[stock_len_];
+    w_price_rate_[0] = 0;
     for (int i = 1; i < stock_len_; ++i) {
-        w_rate_[i] = (w_price_[i] - w_price_[i - 1]) / w_price_[i - 1];
+        w_price_rate_[i] = (w_price_[i] - w_price_[i - 1]) / w_price_[i - 1];
     }
 
     // 计算平均值
-    double w_rate_ma_[stock_len_];
+    cout << "计算 w_price_rate_ma_ ..." << endl;
+    double w_price_rate_ma_[stock_len_];
     int n_ = ma_len_;
     for (int i = 0; i < ma_len_ - 1; ++i) {
-        w_rate_ma_[i] = 0;
+        w_price_rate_ma_[i] = 0;
     }
     while (n_ <= stock_len_) {
         double temp = 0;
         for (int i = n_ - ma_len_; i < ma_len_; ++i) {
-            temp += w_rate_[i];
+            temp += w_price_rate_[i];
         }
-        w_rate_ma_[n_ - 1] = temp / ma_len_;
+        w_price_rate_ma_[n_ - 1] = temp / ma_len_;
         n_++;
     }
 
     // 存入文件
-    ofstream f_w_rate_ma_("c:\\w_rate_ma_.csv");
-    f_w_rate_ma_ << "index" << ","
+    cout << "存档 c:\\w_rate_ma_.csv ..." << endl;
+    ofstream f_w_price_rate_ma_("c:\\w_rate_ma_.csv");
+    f_w_price_rate_ma_
+    << "index" << ","
     << "date" << ","
     << "open" << ","
     << "high" << ","
@@ -78,7 +81,8 @@ IO_DLL_ int w_rate_ma_(
     << "w_rate_" << ","
     << "w_rate_ma_" << "," << endl;
     for (int i = 0; i < stock_len_; i++) {
-        f_w_rate_ma_ << i << ","
+        f_w_price_rate_ma_
+        << i << ","
         << 0 << ","
         << stock_open_[i] << ","
         << stock_high_[i] << ","
@@ -86,9 +90,9 @@ IO_DLL_ int w_rate_ma_(
         << stock_close_[i] << ","
         << 0 << ","
         << w_price_[i] << ","
-        << w_rate_[i] << ","
-        << w_rate_ma_[i] << "," << endl;
+        << w_price_rate_[i] << ","
+        << w_price_rate_ma_[i] << "," << endl;
     }
-    f_w_rate_ma_.close();
+    f_w_price_rate_ma_.close();
     return 0;
 }
